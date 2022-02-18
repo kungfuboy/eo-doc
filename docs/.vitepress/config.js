dc9bbfeb3124765a5543b64941bc2a6080e7a1e2
夜鹰 <17kungfuboy@gmail.com>
2022-02-18 20:34:19 +0800

    set demo link

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  title: 'EoApi',
-  description: 'Just playing around.',
-  base: '/eo-doc',
+  title: "EoApi",
+  description: "Just playing around.",
+  base: "/eo-doc",
   themeConfig: {
     //   头部导航
     nav: [
-      { text: '首页', link: '/' },
-      { text: 'Demo', link: '/demo' },
-      { text: '关于', link: '/about' },
-    ],
-  },
-}
+      { text: "首页", link: "/" },
+      { text: "文档", link: "/about" },
+      { text: "Demo", link: "https://eoapi.vercel.app/" }
+    ]
+  }
+};
